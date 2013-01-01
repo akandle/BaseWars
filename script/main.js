0fdc67fb14c03fb04c007c621c4f067b16a04199
Klaze <akandle@gmail.com>
2012-12-31 23:38:45 -0800

    Almost complete adjusting source files to begin

--- script/main.js
+++ script/main.js
@@ -2,15 +2,14 @@
 //Setting important variables
 var canvas = document.getElementById('surface');
 var ctx = canvas.getContext('2d');
-var game = new EvilAliens();
+var game = new BaseWar();
 var ASSET_MANAGER = new AssetManager();
 
 //Preload all images and sounds
-ASSET_MANAGER.queueDownload('img/alien-explosion.png');
-ASSET_MANAGER.queueDownload('img/alien.png');
+ASSET_MANAGER.queueDownload('img/green-base.png');
+ASSET_MANAGER.queueDownload('img/red-base.png');
 ASSET_MANAGER.queueDownload('img/bullet.png');
-ASSET_MANAGER.queueDownload('img/earth.png');
-ASSET_MANAGER.queueDownload('img/sentry.png');
+ASSET_MANAGER.queueDownload('img/neutral-base.png');
 ASSET_MANAGER.queueDownload('img/explosion.png');
 ASSET_MANAGER.queueSound('alien-boom', 'audio/alien_boom.mp3');
 ASSET_MANAGER.queueSound('bullet-boom', 'audio/bullet_boom.mp3');
